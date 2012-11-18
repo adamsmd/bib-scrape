deb81a28a73251f197a8f627e342bcbd69b85bec
Michael D. Adams <http://www.cs.indiana.edu/~adamsmd/>
2012-11-17 21:18:34 -0800

    Update documentation and factor name handling into a separate file.

--- bib-scrape.pl
+++ bib-scrape.pl
@@ -11,87 +11,122 @@ use Text::BibTeX::Fix;
 use Text::BibTeX::Name;
 use Text::BibTeX::Scrape;
 
-$main::VERSION='1.1';
+$main::VERSION='1.1.2';
 
 =head1 SYNOPSIS
 
 bib-scrape.pl [options] <url> ...
 
-=head2 URL
+=head2 INPUTS
+
+=item <url>
 
 The url of the publisher's page for the paper to be scraped.
-
-Standard URL formats such as 'http://...' are allowed,
-but so is the format 'doi:...'.
-
+Standard URL formats such as 'http://...' can be used.
+The non-standard URL format 'doi:...' can also be used.
 May be prefixed with '{key}' in order to specify an explicit key.
 
-=head2 OPTIONS
+=item --input=<file>
 
-=item --bibtex
+Take BibTeX data from <file> to rescrape or fix.
+If <file> is '-', then read from STDIN.
 
-=item --no-bibtex
+WARNING: "junk" and malformed entities will be omitted from the output
+(This is an upstream problem with the libraries we use.)
 
-Take input as BibTeX data from standard input in addition to the
-default of taking input as URLs from the command line.
-[default=no]
+=item --names=<file>
 
-=item --debug
+Add <file> to the list of name files used to canonicalize author names.
+If <file> is the empty string, clears the list.
 
-=item --no-debug
+See the L</NAME FILE> section for details on the format of name files.
 
-Print debug data (TODO: make it verbose and go to STDERR)
-[default=no]
+=head2 OPERATING MODES
 
-=item --omit=field
+=item --debug, --no-debug [default=no]
 
-Omit a particular field from the output.
+Print debug data
 
-=item --omit-empty=field
+=item --fix, --no-fix [default=yes]
 
-Omit a particular field from the output if it is empty.
+Fix common mistakes in the BibTeX
 
-=item --comma
+=item --scrape, --no-scrape [default=yes]
 
-=item --no-comma
+Scrape BibTeX entry from the publisher's page
 
-Whether to place a comma after the final field of each BibTeX entry.
-[default=yes]
+=head2 GENERAL OPTIONS
 
-=item --generate-keys
+=item --isbn13=<mode> [default=0]
 
-=item --no-generate-keys
+If <mode> is a positive integer, then always use ISBN-13 in the output.
+If negative, then use ISBN-10 when possible.
+If zero, then preserve the original format of an ISBN.
 
-[default=yes]
+=item --isbn-sep=<sep> [default=-]
 
-=item --isbn13=INT
+Use <sep> to separate parts of an ISBN.
+Use an empty string to specify no separator.
 
-=item --isbn-sep=STR
+=item --issn=<kind> [default=both]
 
-[default=-]
+When both a print and an online ISSN are available, use only the print
+ISSN if <kind> is 'print, only the online ISSN if <kind> is 'online',
+or both if <kind> is 'both'.
 
-=item --escape-acronyms
+=item --comma, --no-comma [default=yes]
 
-=item --no-escape-acronyms
+Place a comma after the final field of a BibTeX entry.
 
-[default=yes]
+=item --escape-acronyms, --no-escape-acronyms [default=yes]
+
+In titles, enclose sequences of two or more uppercase letters (i.e.,
+an acronym) in braces to that BibTeX preserves their case.
+
+=head2 Per FIELD OPTIONS
+
+=item --field=STR
 
 =item --no-encode=STR
+
+(TODO: --no-no-encode???)
 
 =item --no-collapse=STR
 
 (TODO: remove no-collapse?)
 
-=item --field=STR
+=item --omit=<field>
 
-=item --input=FILE
+Omit a particular field from the output.
 
-FILE may be "-" to indicate stdin
+=item --omit-empty=<field>
 
-WARNING: "junk" and malformed entities will be omitted from the output
-(This is an upstream problem with the libraries we use.)
+Omit a particular field from the output if it is empty.
+
+=head2 NAME FILES
+
+A name file specifies the correct form for author names.
+Any name that is not of the form "FIRST LAST" is suspect unless
+it is in a name file.
+
+A name file is plain text in Unicode format.
+In a name file, any line starting with # is a comment.
+Blank or whitespace-only lines separate blocks, and
+blocks consist of one or more lines.
+The first line is the canonical form of a name.
+Lines other than the first one are aliases that should be converted to the
+canonical form.
+
+Names are generally matched case insensitively, but note that
+BibTeX uses case to determine the extent of the "van/von" part of a name.
+Thus in some cases like a publisher using "Van Noort" instead of
+"van Noort", you may have to include both cases in the name file.
+
+The default name file provides several examples and recommended practices.
 
 =cut
+
+# TODO: nouns file (one per line, including renames but not case insensitivity?)
 
 ############
 # Options
@@ -138,40 +173,43 @@ sub string_flag {
      "no-$name=s" => sub { delete $HASH->{$_[1]} });
 }
 
-my @valid_names = ([]);
-for (<DATA>) {
-    chomp;
-    if (m/^#/) { }
-    elsif (m/^\s*$/) { push @valid_names, [] }
-    else { push @{$valid_names[$#valid_names]}, new Text::BibTeX::Name($_) }
-}
-@valid_names = map { @{$_} ? ($_) : () } @valid_names;
-
-my ($DEBUG, $SCRAPE, $KEEP_OLD, $FIX) =
-   (      0,      1,         1,    1);
-my ($GENERATE_KEY, $ISBN13, $ISBN_SEP, $ISSN, $COMMA, $ESCAPE_ACRONYMS) =
-   (             1,       0,      '-','both',      1,                1);
+my ($DEBUG, $SCRAPE, $FIX) =
+   (      0,      1,    1);
+my ($ISBN13, $ISBN_SEP, $ISSN, $COMMA, $ESCAPE_ACRONYMS) =
+   (      0,       '-','both',      1,                1);
+my (@NAME_FILE) = ('names.txt');
 my (@INPUT, @EXTRA_FIELDS, %NO_ENCODE, %NO_COLLAPSE, %OMIT, %OMIT_EMPTY);
 
 GetOptions(
+    # Input options
+    'input=s' => sub { push @INPUT, $_[1] },
+    'names=s' => sub { if ($_[1] eq '') { @NAME_FILE=() }
+                       else { push @NAME_FILE, $_[1] } },
+
+    # Operating modes
+    # TODO: make debug be verbose and go to STDERR
     'debug!' => \$DEBUG,
     'fix!' => \$FIX,
-    'field=s' => sub { push @EXTRA_FIELDS, $_[1] },
-    'input=s' => sub { push @INPUT, $_[1] },
-#    #no-defaults
+    'scrape!' => \$SCRAPE,
+
+    # General options
+    # TODO: no-defaults
     'isbn13=i' => \$ISBN13,
     'isbn-sep=s' => \$ISBN_SEP,
     'issn' => \$ISSN,
     'comma!' => \$COMMA,
+    'escape-acronyms!' => \$ESCAPE_ACRONYMS,
+
+    # Field specific options
+    'field=s' => sub { push @EXTRA_FIELDS, $_[1] },
     string_flag('no-encode', \%NO_ENCODE),
-    string_flag('no-collapse', \%NO_COLLAPSE), # Whether to collapse contingues whitespace
+    string_flag('no-collapse', \%NO_COLLAPSE), # Whether to collapse contiguous whitespace
     string_flag('omit', \%OMIT),
-    string_flag('omit_empty', \%OMIT_EMPTY),
-    'escape-acronyms!' => \$ESCAPE_ACRONYMS
+    string_flag('omit-empty', \%OMIT_EMPTY),
     );
 
 my $fixer = Text::BibTeX::Fix->new(
-    valid_names => [@valid_names],
+    valid_names => [map {read_valid_names($_)} @NAME_FILE],
     debug => $DEBUG,
     known_fields => [@EXTRA_FIELDS],
     isbn13 => $ISBN13,
@@ -183,9 +221,6 @@ my $fixer = Text::BibTeX::Fix->new(
     omit => \%OMIT,
     omit_empty => \%OMIT_EMPTY,
     escape_acronyms => $ESCAPE_ACRONYMS);
-# ? ISSN (Print, Online, Both)
-# preserve key if from bib-tex?
-# warn about duplicate author names
 
 # TODO: whether to re-scrape bibtex
 for my $filename (@INPUT) {
@@ -200,6 +235,7 @@ for my $filename (@INPUT) {
             print $entry->print_s;
         } else {
             if (not $entry->exists('bib_scrape_url')) {
+                # Try to find a URL to scrape
                 if ($entry->exists('doi') and $entry->get('doi') =~ m[http://[^/]+/(.*)]i) {
                     (my $url = $1) =~ s[DOI:\s*][]ig;
                     $entry->set('bib_scrape_url', "http://dx.doi.org/$url");
@@ -224,201 +260,24 @@ for (@ARGV) {
 sub scrape_and_fix_entry {
     my ($old_entry) = @_;
 
-#    warn if not exists bib_scrape_url
+    # TODO: warn if not exists bib_scrape_url
     my $entry = (($old_entry->exists('bib_scrape_url') && $SCRAPE) ?
         Text::BibTeX::Scrape::scrape($old_entry->get('bib_scrape_url')) :
         $old_entry);
-#    print $entry->print_s, "\n";
     $entry->set_key($old_entry->key());
-# ALWAYS_GEN_KEY
-#no-SCRAPE = 0, 0, 0
-#SCRAPE = 1, 1, 1
-#    if ($KEEP_OLD) {
-#$PREFER_NEW 1
-#$ADD_NEW 1
-#$REMOVE_OLD 1
     print $FIX ? $fixer->fix($entry) : $entry->print_s;
 }
 
-__DATA__
-
-# Often publishers leave out the middle name even when it is a critial
-# part of the name
-
-Oliveira, Bruno C. d. S.
-Oliveira, Bruno
-
-Dybvig, R. Kent
-Dybvig, R.
-
-# We can't assume a three part name is "first middle last" as some
-# people have two words in their last name.  Further, publishers often
-# get this wrong (e.g. "Jones, Simon Peyton") Thus we explicitly
-# specify those names.
-
-Rodriguez Yakushev, Alexey
-
-Peyton Jones, Simon
-
-Magalhães, José Pedro
-
-Hernán, Miguel Ángel
-
-Klop, Jan Willem
-
-Hove, Siw Elisabeth
-
-Strogatz, Steven H.
-
-# Be careful with last names containing "van", "von", "di", "de", etc.
-# Sometimes these parts of the names are capitalized, while other
-# times they are not.  Always double check how the particular author
-# spells it.
-#
-# Note that when they are lower case, bibtex treats them as a seperate
-# part of the name, but when they are upper case, bibtex treats them
-# as part of the last name.  Thus sometimes case insensitive match
-# isn't enough to find the name.
-
-van Straaten, Anton
-Van Straaten, Anton
-
-van Noort, Thomas
-Van Noort, Thomas
-
-Van Horn, David
-
-Van Hentenryck, Pascal
-
-Di Gianantonio, Pietro
-
-de Paiva, Valeria
-
-De Bosschere, Koen
-
-DeBiasio, Louis
-
-# Sometimes the abbreviated form of a name is what was actually written
-# on the article, but other times it's due to the publisher reporting
-# the wrong data.  Here we can correct the publisher's error, mark the
-# abriviation as correct, or use "[]" to give the full name.  (You can
-# also just change it to the full name but then your bibliography
-# would be lying if the original paper used the abbreviated name.)
-
-Kierstead, H. A.
-Kierstead, H.A.
-
-Frigo, Matteo
-Frigo, M.
-
-Johnson, Steven G.
-Johnson, S.G.
-
-Rawlings, Christopher J.
-Rawlings, C.J.
-
-Clark, Dominic A.
-Clark, D.A.
-
-Barton, Geoffrey J.
-Barton, G.J.
-
-Archer, Iain
-Archer, I.
-
-Saldanha, José W.
-Saldanha, J.W.
-
-Singh, Gulab
-Singh, G.
-
-Venkataraman, G.
-
-kumar (\emph{sic}), V.
-kumar, V.
-
-Rao, Y. S.
-Rao, Y.S.
-
-Snehmani
-
-Riordon, J. S.
-
-Aho, A[lfred] V.
-Aho, A. V.
-
-Hopcroft, J[ohn] E.
-Hopcroft, J. E.
-
-Ullman, J[effrey] D.
-Ullman, J. D.
-
-Alhadidi, D[ima]
-Alhadidi, D.
-
-Belblidia, N[adia]
-Belblidia, N.
-
-Debbabi, M[ourad]
-Debbabi, M.
-
-Bhattacharya, P[rabir]
-Bhattacharya, P.
-
-Streicher, Th[omas]
-Streicher, Th.
-
-Reus, B[ernhard]
-Reus, B.
-
-# Cambridge press returns names in all upper case.  This forces us to
-# list lots of extra names that are in the "first last" form we would
-# normally automatically recognize.
-
-McBride, Conor
-
-McKinna, James
-
-Hinze, Ralf
-
-Jeuring, Johan
-
-Uustalu, Tarmo
-
-Wazny, Jeremy
-
-Kameyama, Yukiyoshi
-
-Kiselyov, Oleg
-
-Shan, Chung-chieh
-
-Holdermans, Stefan
-
-Heeren, Bastiaan
-
-Carette, Jacques
-
-Cebrián, Toni
-
-Fischer, Sebastian
-
-Arbiser, Ariel
-
-Miquel, Alexandre
-
-Ríos, Alejandro
-
-Sperber, Michael
-
-Flatt, Matthew
-
-Findler, Robby
-
-Matthews, Jacob
-
-Gibbons, Jeremy
-
-Chitil, Olaf
-
-Nykänen, Matti
+sub read_valid_names {
+    my ($name_file) = @_;
+    open(NAME_FILE, "<", $name_file) || die "Could not open name file '$name_file': $!";
+    my @names = ([]);
+    for (<NAME_FILE>) {
+        chomp;
+        if (m/^#/) { }
+        elsif (m/^\s*$/) { push @names, [] }
+        else { push @{$names[$#names]}, new Text::BibTeX::Name($_) }
+    }
+    close NAME_FILE;
+    return map { @{$_} ? ($_) : () } @names;
+}
